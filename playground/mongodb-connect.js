e818e8aceef1ae1cff77a75cda644c8338b7e709
himanshu <himanshumehta1114@gmail.com>
2017-10-29 01:35:00 +0530

    Added one document

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,5 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   }
   console.log('Connected to mongo server');
 
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  },(err,result) => {
+    if(err){
+      return console.log('Unable to connect to database',err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
   db.close();
 });
