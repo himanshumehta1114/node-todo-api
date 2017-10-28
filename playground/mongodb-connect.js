d75db1fe05b83306f1ba45a1e650b4a951752798
himanshu <himanshumehta1114@gmail.com>
2017-10-29 01:52:14 +0530

    Destructuring object

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,17 +1,34 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+
+// ObjectID lets us create object id's on the fly
+const {MongoClient, ObjectID} = require('mongodb'); // this is object destructuring from ES6
+
+var obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   if(err) {
     console.log('Unable to connect to mongoDB server');
   }
   console.log('Connected to mongo server');
+  //
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // },(err,result) => {
+  //   if(err){
+  //     return console.log('Unable to connect to database',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-  db.collection('Todos').insertOne({
-    text: 'Something to do',
-    completed: false
-  },(err,result) => {
+  db.collection('Users').insertOne({
+    name: 'Himanshu Mehta',
+    age: 20,
+    location: 'New Delhi'
+  } , (err, result) => {
     if(err){
-      return console.log('Unable to connect to database',err);
+      return console.log('Unable to connect',err);
     }
     console.log(JSON.stringify(result.ops, undefined, 2));
   });
