da2b92a19cc3e826fc4ede5604ea285e825824c0
himanshu <himanshumehta1114@gmail.com>
2017-11-03 23:43:08 +0530

    Hashing by cypto-js

---
+++ playground/hashing.js
@@ -0,0 +1,7 @@
+const {SHA256} = require('crypto-js');
+
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
