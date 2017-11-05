dbc036db070be8b608d43121375e9665edf369f9
himanshu <himanshumehta1114@gmail.com>
2017-11-05 11:30:20 +0530

    hashing and comparing password using bcrypt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,8 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+// using jwt
 var data = {
   id:10,
 }
@@ -10,6 +12,24 @@ console.log(token);
 
 var decoded = jwt.verify(token, 'abc123');
 console.log(decoded);
+
+// using bcrypt
+var password = "123abc!";
+
+// generate salt and hash password
+bcrypt.genSalt(10, (err,salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+// comparing hashed password and plain text password
+var hashedPassword = '$2a$10$vwUt9mhpMVa14AU3V0rIjOK75AR0pQGIkmMftIabgy8zaAr74HSMO';
+
+bcrypt.compare(password, hashedPassword, (err,res) => {
+  console.log(res);
+});
+
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
